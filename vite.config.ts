e47432bf9abe232c711b2a92274b6783b8ba403f
Paulius <sirnimu@users.noreply.github.com>
2023-02-15 23:01:27 +0200

    Remove eslint to avoid errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import eslint from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -13,7 +12,6 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
-    eslint(),
   ],
   esbuild: {
     logOverride: { "this-is-undefined-in-esm": "silent" },
